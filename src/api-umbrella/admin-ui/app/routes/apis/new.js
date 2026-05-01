a4a4e116ccd7cbbfe315fdb75806cb0d5df5574e
Krueger, John <johnk@linux.com>
2026-05-01 13:05:42 -0400

    set up duplication flow into api
    both the link in the template and the bigger changes in the routes file we'll need

--- src/api-umbrella/admin-ui/app/routes/apis/new.js
+++ src/api-umbrella/admin-ui/app/routes/apis/new.js
@@ -1,15 +1,38 @@
 import { inject as service } from '@ember/service';
+import { success } from '@pnotify/core';
 import { clearStoreCache } from 'api-umbrella-admin-ui/utils/uncached-model';
 
 import Form from './form';
 
 export default class NewRoute extends Form {
   @service store;
+  @service duplicateRecord;
 
-  model() {
+  queryParams = {
+    duplicate_id: { refreshModel: true },
+  };
+
+  async model(params) {
     clearStoreCache(this.store);
-    return this.fetchModels(this.store.createRecord('api', {
-      frontendHost: location.hostname,
-    }));
+    let record;
+    if (params.duplicate_id) {
+      record = await this.duplicateRecord.cloneFromId('api', params.duplicate_id);
+    } else {
+      record = this.store.createRecord('api', {
+        frontendHost: location.hostname,
+      });
+    }
+    return this.fetchModels(record);
+  }
+
+  afterModel(resolved) {
+    const record = resolved && resolved.record;
+    if (record && record._duplicatedFromName) {
+      success({
+        title: 'Duplicated',
+        text: `Duplicated from ${record._duplicatedFromName}`,
+      });
+      record._duplicatedFromName = null;
+    }
   }
 }
